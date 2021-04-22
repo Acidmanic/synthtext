9299b3a1241fb5ea5624ed984a637814a987f243
Mani Moayedi <you@example.com>
2021-04-22 07:38:24 +0430

    Add application structure

---
+++ src/main/java/com/acidmanic/synthtext/ApplicationRegistery.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.acidmanic.synthtext;
+
+import com.acidmanic.commandline.commands.Help;
+import com.acidmanic.commandline.commands.TypeRegistery;
+
+/**
+ *
+ * @author diego
+ */
+public class ApplicationRegistery extends TypeRegistery {
+
+    public ApplicationRegistery() {
+
+        this.registerClass(Help.class);
+    }
+
+}
---
+++ src/main/java/com/acidmanic/synthtext/Main.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.acidmanic.synthtext;
+
+import com.acidmanic.commandline.commands.Command;
+import com.acidmanic.commandline.commands.CommandFactory;
+import java.util.HashMap;
+
+/**
+ *
+ * @author diego
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+        ApplicationRegistery registery = new ApplicationRegistery();
+
+        CommandFactory factory = new CommandFactory(registery);
+
+        HashMap<Command, String[]> commands = factory.make(args, true);
+
+        commands.forEach((c, ar) -> c.execute(ar));
+
+    }
+}
